3dbb7406cbd09153033bb75437b0deb823bf7ac9
Agnieszka Pastuszczak <agnieszka.zajac@xfive.co>
2020-07-16 16:17:03 +0200

    Do not save Auth0 login confirmmation page in Session as after sign in/sign up redirect

--- app/controllers/custom/users/auth0_pages_controller.rb
+++ app/controllers/custom/users/auth0_pages_controller.rb
@@ -2,4 +2,7 @@ class Custom::Users::Auth0PagesController < ApplicationController
   skip_authorization_check
 
   def confirm_login; end
+
+  # do not store pages from this controller for after sign in/up redirect
+  def store_location_for(resource_or_scope, location); end
 end
--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -1,9 +1,10 @@
 require "rails_helper"
 
 describe "Sessions" do
+  let(:debate) { create(:debate) }
+
   scenario "Staying in the same page after doing login/logout" do
     user = create(:user, sign_in_count: 10)
-    debate = create(:debate)
 
     visit debate_path(debate)
 
@@ -16,5 +17,50 @@ describe "Sessions" do
 
     expect(page).to have_content("You have been signed out successfully")
     expect(page).to have_current_path(debate_path(debate))
+  end
+
+  context "with Auth0 login enabled" do
+    let(:auth0_hash_with_verified_email) do
+      {
+        provider: "auth0",
+        uid: "12345",
+        info: {
+          name: "manuela",
+          email: "manuelacarmena@example.com",
+          verified: "1"
+        }
+      }
+    end
+
+    before do
+      Setting["feature.auth0_login"] = true
+      OmniAuth.config.add_mock(:auth0, auth0_hash_with_verified_email)
+    end
+
+    after do
+      Setting["feature.auth0_login"] = false
+    end
+
+    scenario "Staying in the same page after doing login/logout" do
+      visit debate_path(debate)
+
+      visit "/users/sessions/confirm_login"
+      click_link "Sign in with Auth0"
+
+      expect_to_be_signed_in
+      expect(page).to have_current_path(debate_path(debate))
+
+      click_link "Sign out"
+
+      expect(page).to have_content("You have been signed out successfully")
+      expect(page).to have_current_path(debate_path(debate))
+    end
+
+    scenario "Redirecting to WP Sign after clicking on Sign in button" do
+      visit "/"
+      click_link "Sign in"
+
+      expect(page).to have_current_path("#{ENV["WORDPRESS_SIGN_UP_URL"]}?redirect_uri=#{confirm_login_url}")
+    end
   end
 end
