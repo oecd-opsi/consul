7112f03e507484edfdd0efee4d43ebd8f9ef2c88
Jakub Bogucki <jakubb.bogucki@xfive.co>
2020-07-20 15:13:03 +0200

    Add newsletter form

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -7,12 +7,14 @@
 
 //= require custom/header
 //= require custom/redirect-click
+//= require custom/newsletter-form
 
 var initializeCustomModules = function () {
   'use strict';
 
   App.CustomHeader.initialize();
   App.CustomRedirectClick.initialize();
+  App.CustomNewsletterForm.initialize();
 };
 
 $(function () {
