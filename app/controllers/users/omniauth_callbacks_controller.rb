9506656e1e64499b89c07d4de18381327015c2cd
Agnieszka Pastuszczak <agnieszka.zajac@xfive.co>
2020-07-20 14:00:05 +0200

    Restore original OmniauthCallbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,10 +15,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     sign_in_with :wordpress_login, :wordpress_oauth2
   end
 
-  def auth0
-    sign_in_with :auth0_login, :auth0
-  end
-
   def after_sign_in_path_for(resource)
     if resource.registering_with_oauth
       finish_signup_path
