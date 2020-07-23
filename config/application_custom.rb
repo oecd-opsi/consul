158de3184f2d98e1fee4775ecef67f5a3cd98964
Agnieszka Pastuszczak <agnieszka.zajac@xfive.co>
2020-07-23 12:02:08 +0200

    Add Sentry config

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,10 @@
 module Consul
   class Application < Rails::Application
+    if ENV["SENTRY_DSN"]
+      Raven.configure do |config|
+        config.dsn = ENV["SENTRY_DSN"]
+        config.environment = ENV["SENTRY_ENV"]
+      end
+    end
   end
 end
