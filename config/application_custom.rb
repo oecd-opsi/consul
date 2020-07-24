ee569ef74eedee35cb4bbbb0c1cc4aa41e4b4c07
Agnieszka Pastuszczak <agnieszka.zajac@xfive.co>
2020-07-24 07:07:19 +0200

    Remove unnecessary sentry environment setting

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -3,7 +3,6 @@ module Consul
     if ENV["SENTRY_DSN"]
       Raven.configure do |config|
         config.dsn = ENV["SENTRY_DSN"]
-        config.environment = ENV["SENTRY_ENV"]
       end
     end
   end
