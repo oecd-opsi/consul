a6e11802dbc25c974385dd0b266a999cfd09330a
Agnieszka Pastuszczak <agnieszka.zajac@xfive.co>
2021-05-12 14:32:34 +0200

    [#41] Add email notification about new Oecd Representative request

---
+++ spec/mailers/custom/notifications_mailer_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+describe Custom::NotificationsMailer do
+  describe "#new_oecd_representative_request" do
+    let(:request) { create(:oecd_representative_request) }
+    let(:email) { Custom::NotificationsMailer.new_oecd_representative_request(user.id, request.id) }
+
+    context "when recipient is an admin" do
+      let(:user) { create(:administrator).user }
+
+      it "sets correct subject " do
+        expect(email.subject).to eq(I18n.t("mailers.oecd_representative_request.subject"))
+      end
+
+      it "includes correct resource link" do
+        expect(email.body).to include(admin_oecd_representative_request_url(request))
+      end
+    end
+
+    context "when recipient is an manager" do
+      let(:user) { create(:manager).user }
+
+      it "sets correct subject " do
+        expect(email.subject).to eq(I18n.t("mailers.oecd_representative_request.subject"))
+      end
+
+      it "includes correct resource link" do
+        expect(email.body).to include(management_oecd_representative_request_url(request))
+      end
+    end
+  end
+end
