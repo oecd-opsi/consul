3f708718110f4474d69793db848d9a98b1ce14b3
Agnieszka Pastuszczak <agnieszka.zajac@xfive.co>
2021-05-18 14:32:53 +0200

    [#41] Does not allow manager to comment as moderator

--- app/models/abilities/manager.rb
+++ app/models/abilities/manager.rb
@@ -17,9 +17,6 @@ module Abilities
       end
 
       merge Abilities::Moderation.new(user)
-
-      can :comment_as_moderator, [Debate, Comment, Proposal, Budget::Investment, Poll::Question,
-                                  Legislation::Question, Legislation::Annotation, Legislation::Proposal, Topic]
     end
   end
 end
