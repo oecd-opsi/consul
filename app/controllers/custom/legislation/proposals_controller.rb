9d784633a8f5bec7d2526a0e6b289ad04edad39d
Agnieszka Pastuszczak <agnieszka.zajac@xfive.co>
2021-06-09 12:31:52 +0200

    [#61] Hide "Scope of operation" field

---
+++ app/controllers/custom/legislation/proposals_controller.rb
@@ -0,0 +1,13 @@
+require_dependency Rails.root.join("app", "controllers", "legislation", "proposals_controller").to_s
+
+class Legislation::ProposalsController < Legislation::BaseController
+  private
+
+    def proposal_params
+      params.require(:legislation_proposal).permit(:legislation_process_id, :title,
+                    :summary, :description, :video_url, :tag_list,
+                    :terms_of_service,
+                    image_attributes: image_attributes,
+                    documents_attributes: [:id, :title, :attachment, :cached_attachment, :user_id])
+    end
+end
